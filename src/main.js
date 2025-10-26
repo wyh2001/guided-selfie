98c12995dac537d02e13ba2c1ffe665662d2cfea
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-10-26 18:18:55 -0400

    Normalize data for debug info, use the centered point of boundingbox to display face position, add size data; add evaluation fn to evaluate data and return user's face position status; add keypoint drawings for potential debugging use; add context fn to reuse data

--- src/main.js
+++ src/main.js
@@ -194,21 +194,63 @@ function handleDetections(detections) {
 	const videoWidth = video.videoWidth;
 	const videoHeight = video.videoHeight;
 	debug.textContent = generateDebugInfo(detections, videoWidth, videoHeight);
-	drawFaceBoxes(detections, videoWidth);
+	drawFaceBoxes(detections, videoWidth, videoHeight);
+	const evals = evaluateFacePosition(detections, videoWidth, videoHeight);
+	evals.forEach((evaluation, index) => {
+		debug.textContent += `Face ${index + 1}: position: ${evaluation.position}, distance: ${evaluation.distance}\n`;
+	});
 }
 
 // https://ai.google.dev/edge/api/mediapipe/js/tasks-vision.boundingbox
+function computeDetectionContext(detection, videoWidth, videoHeight) {
+	const boundingBox = detection.boundingBox;
+	const normalizedCenterX =
+		1 - (boundingBox.originX + boundingBox.width / 2) / videoWidth;
+	const normalizedCenterY =
+		(boundingBox.originY + boundingBox.height / 2) / videoHeight;
+	const normalizedSize =
+		(boundingBox.width * boundingBox.height) / (videoWidth * videoHeight);
+	const normalizedWidth = boundingBox.width / videoWidth;
+	const normalizedHeight = boundingBox.height / videoHeight;
+	const angle = boundingBox.angle;
+	const keypoints = detection.keypoints.map((keypoint) => ({
+		x: keypoint.x,
+		y: keypoint.y,
+	}));
+	return {
+		normalizedCenterX,
+		normalizedCenterY,
+		normalizedSize,
+		normalizedWidth,
+		normalizedHeight,
+		angle,
+		keypoints,
+	};
+}
+
 function generateDebugInfo(detections, videoWidth, videoHeight) {
 	let debugInfo = `Video size: ${videoWidth}x${videoHeight}\n`;
 
 	detections.forEach((detection, index) => {
-		const boundingBox = detection.boundingBox;
-		debugInfo += `Detect face ${index + 1} at [${boundingBox.originX}, ${boundingBox.originY}] with width ${boundingBox.width}, height ${boundingBox.height} and angle ${boundingBox.angle}\n`;
-		const keypoints = detection.keypoints;
+		const {
+			normalizedCenterX,
+			normalizedCenterY,
+			normalizedSize,
+			normalizedWidth,
+			normalizedHeight,
+			angle,
+			keypoints,
+		} = computeDetectionContext(detection, videoWidth, videoHeight);
+		debugInfo += `Detect face ${index + 1} at [${normalizedCenterX.toFixed(2)}, `;
+		debugInfo += `${normalizedCenterY.toFixed(2)}] `;
+		debugInfo += `with size ${normalizedSize.toFixed(2)}, `;
+		debugInfo += `width ${normalizedWidth.toFixed(2)}, `;
+		debugInfo += `height ${normalizedHeight.toFixed(2)} `;
+		debugInfo += `and angle ${angle}\n`;
 		debugInfo += `Keypoints:\n`;
 		keypoints.forEach((keypoint, kpIndex) => {
-			const x = keypoint.x * videoWidth;
-			const y = keypoint.y * videoHeight;
+			const x = keypoint.x;
+			const y = keypoint.y;
 			debugInfo += `keypoint ${kpIndex + 1} at [${x.toFixed(2)}, ${y.toFixed(2)}]\n`;
 		});
 	});
@@ -216,7 +258,7 @@ function generateDebugInfo(detections, videoWidth, videoHeight) {
 }
 
 // Refers to the official MediaPipe Face Detection demo: https://codepen.io/mediapipe-preview/pen/OJByWQr
-function drawFaceBoxes(detections, videoWidth) {
+function drawFaceBoxes(detections, videoWidth, videoHeight) {
 	faceBoxElements.forEach((e) => void e.remove());
 	faceBoxElements.length = 0;
 	const ratio = preview.clientWidth / videoWidth;
@@ -231,7 +273,69 @@ function drawFaceBoxes(detections, videoWidth) {
 		faceBoxElement.style.height = `${boundingBox.height * ratio}px`;
 		preview.appendChild(faceBoxElement);
 		faceBoxElements.push(faceBoxElement);
+
+		const keypoints = detection.keypoints;
+		keypoints.forEach((keypoint, index) => {
+			const keypointElement = document.createElement("div");
+			keypointElement.className = "key-point";
+			keypointElement.style.top = `${keypoint.y * videoHeight * ratio}px`;
+			keypointElement.style.left = `${(1 - keypoint.x) * videoWidth * ratio}px`;
+			keypointElement.textContent = index + 1;
+			preview.appendChild(keypointElement);
+			faceBoxElements.push(keypointElement);
+		});
 	});
+}
+
+const facePosition = {
+	CENTERED: "centered",
+	LEFT: "left",
+	RIGHT: "right",
+	BOTTOM: "bottom",
+	TOP: "top",
+};
+
+const faceDistance = {
+	CLOSE: "close",
+	FAR: "far",
+	NORMAL: "normal",
+};
+
+function evaluateFacePosition(detections, videoWidth, videoHeight) {
+	if (detections.length === 0) {
+		return null;
+	}
+	const evals = [];
+	detections.forEach((detection) => {
+		const { normalizedCenterX, normalizedCenterY, normalizedSize } =
+			computeDetectionContext(detection, videoWidth, videoHeight);
+
+		let position = facePosition.CENTERED;
+		const horizontalOffsetThreshold = 0.1;
+		const positionUpThreshold = 0.5;
+		const positionBottomThreshold = 0.65;
+		if (normalizedCenterX < 0.5 - horizontalOffsetThreshold) {
+			position = facePosition.LEFT;
+		} else if (normalizedCenterX > 0.5 + horizontalOffsetThreshold) {
+			position = facePosition.RIGHT;
+		} else if (normalizedCenterY < positionUpThreshold) {
+			position = facePosition.TOP;
+		} else if (normalizedCenterY > positionBottomThreshold) {
+			position = facePosition.BOTTOM;
+		}
+
+		let distance = faceDistance.NORMAL;
+		const sizeCloseThreshold = 0.2;
+		const sizeFarThreshold = 0.1;
+		if (normalizedSize > sizeCloseThreshold) {
+			distance = faceDistance.CLOSE;
+		} else if (normalizedSize < sizeFarThreshold) {
+			distance = faceDistance.FAR;
+		}
+
+		evals.push({ position, distance });
+	});
+	return evals;
 }
 
 captureBtn.addEventListener("click", async () => {
