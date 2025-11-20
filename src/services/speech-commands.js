4beb4a9c64c3759994048976e812aab8abdbf664
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-11-20 18:02:52 -0500

    Comment out

--- src/services/speech-commands.js
+++ src/services/speech-commands.js
@@ -9,10 +9,10 @@
  * @param {string} command - Command name to dispatch
  */
 function dispatchVoiceCommand(command) {
-  document.dispatchEvent(new CustomEvent('voice:command', {
-    detail: { command }
-  }));
-  console.log('Voice command dispatched:', command);
+  // document.dispatchEvent(new CustomEvent('voice:command', {
+  //   detail: { command }
+  // }));
+  // console.log('Voice command dispatched:', command);
 }
 
 /**
@@ -21,44 +21,22 @@ function dispatchVoiceCommand(command) {
  */
 export function registerDefaultVoiceCommands(manager) {
   // Capture/photo commands
-  manager.registerCommand('take', (transcript) => {
-    if (transcript.includes('photo') || transcript.includes('capture') || transcript.includes('snap')) {
-      dispatchVoiceCommand('take-photo');
-      manager.speak('Photo captured');
-    }
-  });
+  // manager.registerCommand('take', (transcript) => {
+  //   if (transcript.includes('photo') || transcript.includes('capture') || transcript.includes('snap')) {
+  //     dispatchVoiceCommand('take-photo');
+  //     manager.speak('Photo captured');
+  //   }
+  // });
 
-  manager.registerCommand('capture', () => {
-    dispatchVoiceCommand('take-photo');
-    manager.speak('Capturing photo');
-  });
+  // manager.registerCommand('capture', () => {
+  //   dispatchVoiceCommand('take-photo');
+  //   manager.speak('Capturing photo');
+  // });
 
-  manager.registerCommand('snap', () => {
-    dispatchVoiceCommand('take-photo');
-    manager.speak('Snap');
-  });
-
-  // Movement commands
-  manager.registerCommand('left', () => {
-    dispatchVoiceCommand('left');
-    manager.speak('Moving left');
-  });
-
-  manager.registerCommand('right', () => {
-    dispatchVoiceCommand('right');
-    manager.speak('Moving right');
-  });
-
-  // Zoom commands
-  manager.registerCommand('zoom in', () => {
-    dispatchVoiceCommand('zoom-in');
-    manager.speak('Zooming in');
-  });
-
-  manager.registerCommand('zoom out', () => {
-    dispatchVoiceCommand('zoom-out');
-    manager.speak('Zooming out');
-  });
-
-  console.log('Default voice commands registered');
+  // manager.registerCommand('snap', () => {
+  //   dispatchVoiceCommand('take-photo');
+  //   manager.speak('Snap');
+  // });
+  
+  // console.log('Default voice commands registered');
 }
